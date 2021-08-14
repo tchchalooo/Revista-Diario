cd81e84a85e1db28dd0e0ccbc696a71fda96037e
Gonzalo Solis <gonzalo.solisc@hotmail.com>
2021-08-14 16:08:40 -0400

    full proyect uploaded

---
+++ assets/js/scripts.js
@@ -0,0 +1,27 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+/*function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.hamburguesita')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }*/
+
+
+  $(document).ready(function(){
+$("#btn_hamburguesa"). click(function(){
+$("#myDropdown"). toggle();
+
+})
+  });
